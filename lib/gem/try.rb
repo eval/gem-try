bfdc697ddaf57f3308766474fd035182a6f1690c
Gert Goet <gert@thinkcreate.dk>
2024-10-03 13:50:43 +0200

    Generated
    ...with `bundle gem gem-try`

---
+++ lib/gem/try.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "try/version"
+
+module Gem
+  module Try
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/gem/try_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Gem::Try do
+  it "has a version number" do
+    expect(Gem::Try::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
